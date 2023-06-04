dfa75f0422023218517d0ac7954c716f6c379e27
TxpGit <1101642762@qq.com>
2023-06-04 16:14:37 +0800

    添加 解决跨域问题，关闭mybatis插件

--- src/main/java/com/example/testdemo/Controller/workCreateController.java
+++ src/main/java/com/example/testdemo/Controller/workCreateController.java
@@ -4,23 +4,20 @@ import com.example.testdemo.Service.workCreateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/createWork")
 @Slf4j
+@CrossOrigin
 public class workCreateController {
 
     @Autowired
     private workCreateService workCreateService;
 
-    @PostMapping("/firstCreate")
-    public ResponseEntity<String> ctrateWork(@RequestBody String requestMessage){
-        log.info("接收到请求:{}",requestMessage);
-        workCreateService.createWork(requestMessage);
+    @GetMapping("/firstCreate")
+    public ResponseEntity<String> ctrateWork(){
+        workCreateService.createWork("requestMessage");
         return ResponseEntity.ok("success");
     }
 }

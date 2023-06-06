805509dd0f9835be7218389920920330f47d2618
TxpGit <1101642762@qq.com>
2023-06-07 00:18:21 +0800

    添加 小兔鲜后台接口实现

--- src/main/java/com/example/testdemo/Controller/XtxVueController.java
+++ src/main/java/com/example/testdemo/Controller/XtxVueController.java
@@ -1,36 +1,39 @@
 package com.example.testdemo.Controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.example.testdemo.Service.XtxVueService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("XtxVueServer")
 @CrossOrigin
+@Slf4j
 public class XtxVueController {
 
+    @Autowired
+    private XtxVueService xtxVueService;
+
+
+    //小兔鲜Head商品分类
     @GetMapping("home/category/head")
     public ResponseEntity<String> getCategoryHead(){
 
-        ArrayList<JSONObject> objects = new ArrayList<>();
-        JSONObject jsonObject0 = new JSONObject();
-        jsonObject0.put("id","1005000");
-        jsonObject0.put("name","居家");
-        jsonObject0.put("picture","http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-05-06/201516e3-25d0-48f5-bcee-7f0cafb14176.png");
-        jsonObject0.put("name","居家");
+        String resultStr = xtxVueService.getHeadCategory();
+        log.info("获取Home分类数据");
+        return ResponseEntity.ok(resultStr);
+    }
 
-        JSONObject jsonObject1 = new JSONObject();
-        jsonObject1.put("id","1005002");
-        jsonObject1.put("name","美食");
-
-        objects.add(jsonObject0);
-        objects.add(jsonObject1);
-        return ResponseEntity.ok(JSON.toJSONString(objects));
+    //小兔鲜Head商品分类
+    @GetMapping("home/banner")
+    public ResponseEntity<String> getBannerHead(){
+        String resultStr = xtxVueService.getBannerHead();
+        log.info("获取Home轮播数据");
+        return ResponseEntity.ok(resultStr);
     }
 }
---
+++ src/main/java/com/example/testdemo/Service/XtxVueService.java
@@ -0,0 +1,8 @@
+package com.example.testdemo.Service;
+
+public interface XtxVueService {
+    String getHeadCategory();
+
+    String getBannerHead();
+
+}
